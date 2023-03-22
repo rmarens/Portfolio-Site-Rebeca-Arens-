714fea2c717884fd22b61e380ec4c21d979c9b89
rmarens <127261642+rmarens@users.noreply.github.com>
2023-03-22 00:16:38 -0400

    Create menu toggle
    Toggle function created for hamburger menu in header (mobile view)

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Hi! Welcome to my portfolio site. Take a look around--I hope you enjoy checking out my work!")
+
+function menuToggle() {
+    var x = document.getElementById("myNavtoggle");
+    if (x.className === "navtoggle") {
+        x.className += " responsive"
+    } else {
+        x.className = "navtoggle";
+    }
+}
